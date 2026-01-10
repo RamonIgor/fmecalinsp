9a4ee07c5b1129ac2943c45cd92fef28dddbc26a
Igor Henrique Ramon <igorhenriqueramon@gmail.com>
2026-01-10 17:01:56 +0000

    o fundo de várias coisas está escuro, inclusive da tela de login, é isso

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -23,7 +23,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="pt-BR" className="dark">
+    <html lang="pt-BR">
       <body className={cn('min-h-screen font-body antialiased', inter.variable)}>
         <FirebaseClientProvider>
           {children}
