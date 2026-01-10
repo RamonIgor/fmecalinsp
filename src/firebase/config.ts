38a7d8f19c52c3715a71ac4552999e71b7210dc9
Igor Henrique Ramon <igorhenriqueramon@gmail.com>
2026-01-10 15:13:06 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-9752559563-5761a",
+  "appId": "1:737791402724:web:54de6696f8c88fe30982f1",
+  "apiKey": "AIzaSyC9fLsOhpNjPXxC-lz_-fAyqp2YkESggho",
+  "authDomain": "studio-9752559563-5761a.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "737791402724"
+};
