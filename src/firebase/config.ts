35cc9c9a6e6238ab672f16ddd515f84a665eaacd
Igor Henrique Ramon <igorhenriqueramon@gmail.com>
2026-01-30 14:52:13 +0000

    veja esse aviso, preciso resolver.

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,8 +1,8 @@
 export const firebaseConfig = {
-  "projectId": "studio-9752559563-5761a",
-  "appId": "1:737791402724:web:54de6696f8c88fe30982f1",
-  "apiKey": "AIzaSyC9fLsOhpNjPXxC-lz_-fAyqp2YkESggho",
-  "authDomain": "studio-9752559563-5761a.firebaseapp.com",
-  "measurementId": "",
-  "messagingSenderId": "737791402724"
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID || "",
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
 };
