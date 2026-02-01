03e40ec63a1eed3f8199936b6ed0cf51b7fcb1d1
Igor Henrique Ramon <igorhenriqueramon@gmail.com>
2026-02-01 14:51:33 +0000

    Estou testando num aparelho celular e está dando erro quando insiro o lo

--- next.config.js
+++ next.config.js
@@ -4,13 +4,64 @@ const withPWA = require("@ducanh2912/next-pwa").default({
   register: true,
   skipWaiting: true,
   runtimeCaching: [
+    // Cache pages with a Network-First strategy.
     {
       urlPattern: ({ request }) => request.mode === "navigate",
       handler: "NetworkFirst",
       options: {
         cacheName: "pages-cache",
+        expiration: {
+          maxEntries: 60,
+          maxAgeSeconds: 30 * 24 * 60 * 60, // 30 days
+        },
       },
     },
+    // Cache static assets (JS, CSS, fonts) with a Stale-While-Revalidate strategy.
+    {
+      urlPattern: /\.(?:js|css|woff2?|ico|png|svg|jpg|jpeg|webp)$/,
+      handler: "StaleWhileRevalidate",
+      options: {
+        cacheName: "static-assets-cache",
+        expiration: {
+          maxEntries: 100,
+          maxAgeSeconds: 7 * 24 * 60 * 60, // 7 days
+        },
+      },
+    },
+    // Cache images from external sources with a Cache-First strategy.
+    {
+      urlPattern: /^https?:\/\/images\.unsplash\.com\/.*/,
+      handler: "CacheFirst",
+      options: {
+        cacheName: "unsplash-images-cache",
+        expiration: {
+          maxEntries: 50,
+          maxAgeSeconds: 30 * 24 * 60 * 60, // 30 days
+        },
+      },
+    },
+    {
+      urlPattern: /^https?:\/\/picsum\.photos\/.*/,
+      handler: "CacheFirst",
+      options: {
+        cacheName: 'picsum-images-cache',
+        expiration: {
+          maxEntries: 50,
+          maxAgeSeconds: 30 * 24 * 60 * 60, // 30 days
+        },
+      },
+    },
+    {
+      urlPattern: /^https?:\/\/placehold\.co\/.*/,
+      handler: "CacheFirst",
+      options: {
+        cacheName: 'placehold-images-cache',
+        expiration: {
+          maxEntries: 50,
+          maxAgeSeconds: 30 * 24 * 60 * 60, // 30 days
+        },
+      },
+    }
   ],
 });
 
