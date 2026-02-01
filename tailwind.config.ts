8f1d71906f881b70581f3efa114f10f4b99dad9a
Igor Henrique Ramon <igorhenriqueramon@gmail.com>
2026-02-01 13:27:49 +0000

    The app isn't starting. Please investigate what could be wrong based on

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,6 +1,7 @@
-import type {Config} from 'tailwindcss';
+import type { Config } from 'tailwindcss';
 
-export default {
+const config: Config = {
+  darkMode: ['class'],
   content: [
     './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
     './src/components/**/*.{js,ts,jsx,tsx,mdx}',
@@ -95,4 +96,6 @@ export default {
     },
   },
   plugins: [require('tailwindcss-animate')],
-} satisfies Config;
+};
+
+export default config;
